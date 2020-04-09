3c53c86d02986bcd20e9341279b022433aa28eaf
松島勇貴 <macho.macho.dev@gmail.com>
2020-04-10 07:19:47 +0900

    LoginViewModel interface

--- lib/viewmodel/login_viewmodel.dart
+++ lib/viewmodel/login_viewmodel.dart
@@ -1,4 +1,22 @@
+import 'dart:async';
 
 class LoginViewModel {
+  // input
+  final _onLoginPageAppearStreamController = StreamController<void>();
+  final _onSignInButtonTappedStreamController = StreamController<void>();
+  StreamSink<void> get onLoginPageAppear => _onLoginPageAppearStreamController.sink;
+  StreamSink<void> get onSignInButtonTapped => _onSignInButtonTappedStreamController.sink;
 
+  // output
+  final _isSignInStreamController = StreamController<bool>();
+  final _errorMessageStreamController = StreamController<String>();
+  Stream<bool> get isSignIn => _isSignInStreamController.stream;
+  Stream<String> get errorMessage => _errorMessageStreamController.stream;
+
+  LoginViewModel() {
+    _bindInputAndOutput();
+  }
+
+  void _bindInputAndOutput() {
+  }
 }
